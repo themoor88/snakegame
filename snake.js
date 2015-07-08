01360c7b1f6f8ba42d6a1b8bbc9f6137e37c7559
Wajid Kagzi <wjkagzi@gmail.com>
2015-07-08 17:54:04 -0400

    Drawing the snake.

--- snake.js
+++ snake.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
+  // Initiate the canvas
   var canvas = $("#canvas")[0];
   var ctx = canvas.getContext("2d");
   var w = $("#canvas").width();
   var h = $("#canvas").height();
+
+  // Painting the canvas
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, w, h);
+  ctx.strokeStyle = "black";
+  ctx.strokeRect(0, 0, w, h);
+
+  drawSnake();
+  // Drawing the snake
+  function drawSnake() {
+    var snakeLength = 5;
+    var snakeArray = [];
+
+    for (var i = snakeLength - 1; i >= 0; i--) {
+      snakeArray.push({x: i, y: 0})
+    };
+  }
 });
